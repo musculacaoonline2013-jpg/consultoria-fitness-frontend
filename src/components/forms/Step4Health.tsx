739c54a9d2774cbc8853e201c40cef6521b823cd
musculacaoonline2013-jpg <musculacaoonline2013@gmail.com>
2025-12-11 08:42:18 -0300

    Update Step4Health.tsx

---
+++ src/components/forms/Step4Health.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import React from 'react';
+
+type Step4HealthProps = {
+  [key: string]: any;
+};
+
+const Step4Health: React.FC<Step4HealthProps> = (props) => {
+  return (
+    <div>
+      {/* Passo 4 - Saúde / restrições */}
+      <p>Formulário - Passo 4 (Saúde)</p>
+    </div>
+  );
+};
+
+export default Step4Health;
