8593e3e672a4d5186b5d090f2bed66afa6b15414
musculacaoonline2013-jpg <musculacaoonline2013@gmail.com>
2025-12-11 08:41:42 -0300

    Update Step2Training.tsx

---
+++ src/components/forms/Step2Training.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import React from 'react';
+
+type Step2TrainingProps = {
+  [key: string]: any;
+};
+
+const Step2Training: React.FC<Step2TrainingProps> = (props) => {
+  return (
+    <div>
+      {/* Passo 2 - Experiência de treino */}
+      <p>Formulário - Passo 2 (Treino)</p>
+    </div>
+  );
+};
+
+export default Step2Training;
