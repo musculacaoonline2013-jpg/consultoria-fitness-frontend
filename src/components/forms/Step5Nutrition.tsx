7aea6669f352ecbd85eaafd03fd148a827d48fc2
musculacaoonline2013-jpg <musculacaoonline2013@gmail.com>
2025-12-11 08:43:51 -0300

    Update Step5Nutrition.tsx

---
+++ src/components/forms/Step5Nutrition.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import React from 'react';
+
+type Step5NutritionProps = {
+  [key: string]: any; // aceita qualquer prop por enquanto
+};
+
+const Step5Nutrition: React.FC<Step5NutritionProps> = (props) => {
+  return (
+    <div>
+      {/* Passo 5 - Nutrição / Preferências alimentares */}
+      <p>Formulário - Passo 5 (Nutrição)</p>
+    </div>
+  );
+};
+
+export default Step5Nutrition;
