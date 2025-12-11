1d32137c79c1dd340cac89d7e4f2adb97d96e2ec
musculacaoonline2013-jpg <musculacaoonline2013@gmail.com>
2025-12-11 08:42:00 -0300

    Update Step3Equipment.tsx

---
+++ src/components/forms/Step3Equipment.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import React from 'react';
+
+type Step3EquipmentProps = {
+  [key: string]: any;
+};
+
+const Step3Equipment: React.FC<Step3EquipmentProps> = (props) => {
+  return (
+    <div>
+      {/* Passo 3 - Equipamentos disponíveis */}
+      <p>Formulário - Passo 3 (Equipamentos)</p>
+    </div>
+  );
+};
+
+export default Step3Equipment;
